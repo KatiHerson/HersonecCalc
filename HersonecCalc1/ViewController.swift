717382e6e21cc7a41276202940beb54fae61010b
Test <Test>
2020-11-09 17:03:51 +0300

    calc

--- HersonecCalc1/ViewController.swift
+++ HersonecCalc1/ViewController.swift
@@ -9,11 +9,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var operand = ""
+    var numberOne = ""
+    var numberTwo = ""
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func inputNumber(_ sender: UIButton) {
+        if operand.isEmpty {
+            numberOne = numberOne + (sender.titleLabel?.text)!
+            resultLabel.text = numberOne
+        } else {
+            numberTwo = numberTwo + (sender.titleLabel?.text)!
+            resultLabel.text = numberTwo
+        }
     }
-
-
+           //Функция обнуления
+    @IBAction func clearAction(_ sender: UIButton) {
+        numberOne = ""
+        numberTwo = ""
+        operand = ""
+        resultLabel.text = "0"
+    }
+    // Функция обработкинажатия на клавиши +, -, *, /
+    @IBAction func inputOperand(_ sender: UIButton) {
+        operand = sender.titleLabel?.text as! String
+    }
+    //Кнопка результата
+    @IBAction func resultAction(_ sender: Any) {
+        var result = 0.0
+       
+        switch operand {
+        case "/":
+            result = Double(numberOne)! /
+            Double(numberTwo)!
+        case "*":
+            result = Double(numberOne)! *
+            Double(numberTwo)!
+        case "-":
+            result = Double(numberOne)! -
+            Double(numberTwo)!
+        case "+":
+            result = Double(numberOne)! +
+            Double(numberTwo)!
+        default:
+            break
+        }
+        
+        if result.truncatingRemainder(dividingBy: 1.0) == 00 {
+            resultLabel.text = String(Int(result))
+        } else {
+            resultLabel.text = String(result)
+        }
+    }
 }
-
